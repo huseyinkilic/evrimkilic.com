2c7b267999e8eb240cac5b28849ff63c68eab945
Huseyin Kilic <huseyin@kilic.org>
2018-05-27 12:39:43 +0200

    Initial commit of static site

--- README.md
+++
@@ -1,1 +0,0 @@
-# evrimkilic.com
---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://www.evrimkilic.com/feed/atom">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://www.evrimkilic.com/feed/atom";
+		</script>
+
+		<p>You are being redirected to <a href="http://www.evrimkilic.com/feed/atom">http://www.evrimkilic.com/feed/atom</a></p>
+	</body>
+</html>
